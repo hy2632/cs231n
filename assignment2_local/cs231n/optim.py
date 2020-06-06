9c8f8cb83b70a157cd5409ba85d2480666dd23c9
Hua <54726816+hy2632@users.noreply.github.com>
2020-06-06 01:33:01 -0400

    6.6

--- assignment2_local/cs231n/optim.py
+++ assignment2_local/cs231n/optim.py
@@ -69,7 +69,9 @@ def sgd_momentum(w, dw, config=None):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    v = config["momentum"] * v - config["learning_rate"] * dw # velocity update stays the same
+    next_w = w + v
+
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -107,7 +109,14 @@ def rmsprop(w, dw, config=None):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    # cache = decay_rate * cache + (1 - decay_rate) * dx**2
+    # x += - learning_rate * dx / (np.sqrt(cache) + eps)
+
+    config["cache"] = config["decay_rate"] * config["cache"] + (
+        1 - config["decay_rate"]) * dw**2
+    next_w = w - config["learning_rate"] * dw / (np.sqrt(config["cache"]) +
+                                                 config["epsilon"])
+
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -152,7 +161,27 @@ def adam(w, dw, config=None):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    # t is your iteration counter going from 1 to infinity
+    learning_rate = config["learning_rate"]
+    beta1 = config["beta1"]
+    beta2 = config["beta2"]
+    eps = config["epsilon"]
+    m = config["m"]
+    v = config["v"]
+    t = config["t"]
+
+    t += 1
+    config["t"] = t
+
+    m = beta1 * m + (1 - beta1) * dw
+    mt = m / (1 - beta1 ** t)
+    v = beta2 * v + (1 - beta2) * (dw ** 2)
+    vt = v / (1 - beta2 ** t)
+
+    config["m"] = m
+    config["v"] = v
+    
+    next_w = w - learning_rate * mt / (np.sqrt(vt) + eps)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
